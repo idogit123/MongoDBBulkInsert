765a36644832c9b31bfb2992cd2ffb22412e18cc
Ido V. Z <programerido@gmail.com>
2024-07-18 00:42:13 +0300

    Insert 10m users

--- MongoDBBulkInsert/BulkInsertOperation.cs
+++ MongoDBBulkInsert/BulkInsertOperation.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Driver;
+using Newtonsoft.Json;
 
 namespace MongoDBBulkInsert;
 
@@ -6,12 +7,38 @@ public class BulkInsertOperation
 {
     public MongoClient client { get; set; }
     public IMongoDatabase db { get; set; }
-    public string collection { get; set; }
+    public IMongoCollection<User> collection { get; set; }
 
     public BulkInsertOperation(string connectionString, string databaseName, string collectionName)
     {
         client = new MongoClient(connectionString);
         db = client.GetDatabase(databaseName);
         collection = db.GetCollection<User>(collectionName);
+    }
+
+    public void BulkInsert(string filePath)
+    {
+        var usersBatch = new List<User>();
+        using var reader = new StreamReader(filePath);
+        string line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            var user = JsonConvert.DeserializeObject<User>(line);
+            usersBatch.Add(user);
+
+            if (usersBatch.Count >= 1000)
+            {
+                collection.InsertMany(usersBatch);
+                usersBatch.Clear();
+            }
+        }
+
+        if (usersBatch.Count > 0)
+        {
+            collection.InsertMany(usersBatch);
+        }
+
+        Console.WriteLine($"Bulk insrt complete for file: {filePath}");
     }
 }
--- MongoDBBulkInsert/Program.cs
+++ MongoDBBulkInsert/Program.cs
@@ -1,5 +1,24 @@
 ﻿
-var CONNECTION_STRING = "mongodb://localhost:27017";
-var DATABASE_NAME = "Users";
-var COLLECTION_NAME = "Users";
+using MongoDBBulkInsert;
+using System.Diagnostics;
 
+var CONNECTION_STRING = "mongodb://localhost:27017";
+var DATABASE_NAME = "users";
+var COLLECTION_NAME = "users";
+var USERS_FILES_DIRECTORY = "C:/Users/Ido/Documents/GitHub/user-search/user-generator/users";
+var USER_FILES_AMOUNT = 10;
+
+var bulkInert = new BulkInsertOperation(CONNECTION_STRING, DATABASE_NAME, COLLECTION_NAME);
+
+var timer = new Stopwatch();
+
+timer.Start();
+for (var fileNumber = 0; fileNumber < USER_FILES_AMOUNT; fileNumber++)
+{
+    var filePath = $"{USERS_FILES_DIRECTORY}/users{fileNumber}.jsonl";
+
+    bulkInert.BulkInsert(filePath);
+}
+timer.Stop();
+
+Console.WriteLine($"Timer: {timer.ToString()}");
